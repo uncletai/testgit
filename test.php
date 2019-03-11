b6d228051bce0d14df3702a4b5ca3ca430cbe78e
ben <beny@ciandt.com>
2019-03-11 11:59:02 +0800

    Add object.php file and update test.php

--- test.php
+++ test.php
@@ -1,16 +1,2 @@
-<?php
-
-class aaa {
-	public $a = 5;
-	public function bbb($a) {
-		echo $a . '</br>';
-	}
-}
-
-$aaa = new aaa();
-
-$b = $aaa->bbb(3);
-
-$a = $aaa->a;
-
-$b2 = $aaa->bbb($a);
+<?php 
+	echo 'This is git test file content.';
