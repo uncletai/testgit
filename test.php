1cc69e4b8012ad5a7e00ad63081b8a4ab6bdb91e
ben <beny@ciandt.com>
2019-03-11 11:03:46 +0800

    Content update to object

--- test.php
+++ test.php
@@ -1,3 +1,16 @@
-asdasdsadsadasx
-dsfsfdd
+<?php
 
+class aaa {
+	public $a = 5;
+	public function bbb($a) {
+		echo $a . '</br>';
+	}
+}
+
+$aaa = new aaa();
+
+$b = $aaa->bbb(3);
+
+$a = $aaa->a;
+
+$b2 = $aaa->bbb($a);
